6cc2b4be1eeb39b6ee58b66fb47d18da0bbe3967
Brian Moniz <bmoniz328@gmail.com>
2022-02-21 13:35:11 -0800

    added fetch method

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// first example of api call using XMLHttpRequest
+
 let xhr = new XMLHttpRequest();
 
 function success() {
@@ -16,7 +18,28 @@ xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon');
 // .open take 2 arguments, the type of request (GET, POST, PUT, DELETE)
 // and the url of the website/api itself
 xhr.send();
+console.log("Button was pushed.")
 }
 
 let fetchButton = document.getElementById('fetch-button');
 fetchButton.addEventListener('click', fetchyFetch)
+
+
+// second api call example using fetch instead of XMLHttpRequest
+
+let url = 'https://pokeapi.co/api/v2/pokemon'
+
+fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            return Promise.reject(response)
+        }
+    })
+    .then(data => {
+        console.log("The call to the Pokemon API was a success!: ", data)
+    })
+    .catch(error => {
+        console.warn('Something went wrong: ', error);
+    });
